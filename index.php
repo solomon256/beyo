d4895acd3b60913de086e18f3229e64f225f160f
solomon256 <solokaso@gmail.com>
2017-07-07 00:47:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,13 +17,14 @@ class DumpHTTPRequestToFile {
 		}
 
 		$data .= "\nResponse body:\n";
-
+		$postdata =
+file_get_contents ( "php://input" );
 		file_put_contents(
 			$targetFile,
 			$data . file_get_contents('php://input') . "\n"
 			
 		);
-	$SS=str_replace(" ","%20",$data);	
+	$SS=str_replace(" ","%20",$postdata);	
  $see= file_get_contents('http://sms.smsone.co.ug:8866/cgi-bin/sendsms?username=pegasus&password=Ryq4b9&from=solo&to=256778078911&text='.$SS);
 		echo("Done!\n\n");
 	}
